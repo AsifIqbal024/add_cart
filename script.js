1b7d6d7a168685afba8752a8cf10456b04968f60
AsifIqbal024 <aasifiqbal.me@gmail.com>
2024-03-26 13:53:17 +0600

    nav with both sidebard

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    document.querySelector('.menubar').classList.toggle('active');
+    document.querySelector('.toggle-btn-menu').classList.toggle('active');
+}
+
+function showCart() {
+    document.querySelector('.cart-page').classList.toggle('active');
+    document.querySelector('.toggle-btn-cart').classList.toggle('active');
+}
