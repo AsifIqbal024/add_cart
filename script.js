75bec1c55c58c14d93e4cda9956491ff092b2de0
Kazi Asif Iqbal <157669092+AsifIqbal024@users.noreply.github.com>
2024-03-26 19:00:57 +0600

    Cart Number Add

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function showCart() {
     document.querySelector('.cart-page').classList.toggle('active');
     document.querySelector('.toggle-btn-cart').classList.toggle('active');
 }
+
+let val = 0 ;
+function cartAdd(){
+    val=val+1;
+    document.querySelector('.cart-total-items').innerHTML=val;
+    document.querySelector('.cartItemTotal').innerHTML = `<p>Your Cart Has ${val} Items</p>`
+}
